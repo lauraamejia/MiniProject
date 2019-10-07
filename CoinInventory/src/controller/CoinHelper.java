d88f14be0e226056e725d5dbe9b0d165ba730fa5
lauraamejia <lauramejiat16@gmail.com>
2019-10-06 19:08:32 -0500

    added transaction helper

--- CoinInventory/src/controller/CoinHelper.java
+++ CoinInventory/src/controller/CoinHelper.java
@@ -12,60 +12,101 @@ import model.Coin;
 public class CoinHelper {
 	
 	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("CoinInventory");
-
-	public void insertNewCoins(Coin s) {
+	
+	public void insertCoin(Coin c) {
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
-		em.persist(s);
+		em.persist(c);
 		em.getTransaction().commit();
 		em.close();
 	}
-
-	public List<Coin> getCoins() {
+	
+	public List<Coin> showAllBuyers(){
 		EntityManager em = emfactory.createEntityManager();
-		List<Coin> allDetails = em.createQuery("SELECT d FROM Coin d").getResultList();
-		return allDetails;
+		List<Coin> allCoins = em.createQuery("SELECT i FROM Coin i").getResultList();
+		return allCoins;
 	}
-
-	public Coin searchForCoinById(Integer tempId) {
-		// TODO Auto-generated method stub
+	
+	public void deleteCoin(Coin toDelete) {
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
-		Coin found = em.find(Coin.class, tempId);
-		em.close();
-		return found;
-	}
-
-	public void deleteItem(Coin coinToDelete) {
-		// TODO Auto-generated method stub
-		EntityManager em = emfactory.createEntityManager();
-		em.getTransaction().begin();
-		TypedQuery<Coin> typedQuery = em.createQuery("select d from Coin d where d.id = :selectedid",Coin.class);
-		// Substitute parameter with actual data from the toDelete item
-		typedQuery.setParameter("selectedid", coinToDelete.getId());
-
-
-		// we only want one result
+		TypedQuery<Coin> typedQuery = em.createQuery("select c from Coin c where c.id = :selectedid and c.contidion = :selectedContidion and c.dateoncoin = :selecteDateOnCoin and c.type = :selectedType", Coin.class);
+		
+		//Substitute parameter with actual data from the toDelete item
+		typedQuery.setParameter("selectedId", toDelete.getId());
+		typedQuery.setParameter("selectedContidion", toDelete.getContidion());
+		typedQuery.setParameter("selectedDateOnCoin", toDelete.getDateOnCoin());
+		typedQuery.setParameter("selectedType", toDelete.getType());
+		
+		//we only want one result
 		typedQuery.setMaxResults(1);
-
-		// get the result and save it into a new list detail object
+		
+		//get the result and save it into a new list item
 		Coin result = typedQuery.getSingleResult();
-
-		// remove it
+		
+		//remove it
 		em.remove(result);
 		em.getTransaction().commit();
 		em.close();
-
 	}
-
+	
+	public Coin searchForCoinById(int idToEdit) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		Coin found = em.find(Coin.class, idToEdit);
+		em.close();
+		return found;
+	}
+	
 	public void updateCoin(Coin toEdit) {
 		// TODO Auto-generated method stub
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
-
+		
 		em.merge(toEdit);
 		em.getTransaction().commit();
 		em.close();
+	}
+	
+	public List<Coin> searchForCoinByContidion(String contidion) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Coin> typedQuery = em.createQuery("select c from Coin c where c.contidion = :selectedContidion", Coin.class);
+		typedQuery.setParameter("selectedContidion", contidion);
+		
+		List<Coin> foundCoin = typedQuery.getResultList();
+		em.close();
+		return foundCoin;
+	}
+	
+	public List<Coin> searchForCoinByStorageLocation(String storageLocation) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Coin> typedQuery = em.createQuery("select c from Coin c where c.storageLocation = :selectedStorageLocation", Coin.class);
+		typedQuery.setParameter("selectedState", storageLocation);
+		
+		List<Coin> foundCoin = typedQuery.getResultList();
+		em.close();
+		return foundCoin;
+	}
+	
+	public List<Coin> searchForCoinrByType(String type) {
+		// TODO Auto-generated method stub
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Coin> typedQuery = em.createQuery("select c from Coin c where c.type = :selectedType", Coin.class);
+		typedQuery.setParameter("selectedFname", type);
+		
+		List<Coin> foundCoin = typedQuery.getResultList();
+		em.close();
+		return foundCoin;
+	}
+	
+	public void cleanUp() {
+		emfactory.close();
 	}
 
 }
---
+++ CoinInventory/src/controller/TransactionHelper.java
@@ -0,0 +1,68 @@
+package controller;
+
+import model.Transaction;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class TransactionHelper {
+
+	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("CoinInventory");
+	
+	
+	public void addTransaction(Transaction toAdd) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(toAdd);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	
+	public Transaction searchForTransactionById(int idToEdit) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		Transaction found = em.find(Transaction.class, idToEdit);
+		em.close();
+		return found;
+	}
+
+	public void updateTransaction(Transaction toEdit) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		em.merge(toEdit);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	public void deleteTransaction(Transaction toDelete) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Transaction> typedQuery = em.createQuery("select ch from Transaction ch where ch.tableid = :selectedTableID and ch.dealer = :selectedDealer and ch.price = :selectedPrice and ch.tranDate = :selectedTranDate", Transaction.class);		
+		typedQuery.setParameter("selectedId", toDelete.getTableID());
+		typedQuery.setParameter("selectedDealer", toDelete.getDealer());
+		typedQuery.setParameter("selectedPrice", toDelete.getPrice());
+		typedQuery.setParameter("selectedTranDate", toDelete.getTranDate());
+		
+		typedQuery.setMaxResults(1);
+		
+		Transaction result = typedQuery.getSingleResult();
+		
+		em.remove(result);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public List<Transaction> showAllTransactions() {
+		EntityManager em = emfactory.createEntityManager();
+		List<Transaction> allItems = em.createQuery("SELECT i FROM Transaction i").getResultList();
+		return allItems;
+	}
+
+	public void cleanUp() {
+		emfactory.close();
+	}
+}
